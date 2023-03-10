c23862dfe12ce50eb9d891ff500a305526a9011c
李心宇 <873720870@qq.com>
2023-03-10 17:31:45 +0800

    Changes

--- GEA.py
+++ GEA.py
@@ -13,18 +13,24 @@ df['ds'] = pd.to_datetime(df['date'])
 df['y'] = df['demand']
 df = df[['ds', 'y']]
 
+holidays = pd.DataFrame({
+  'holiday': ['春节', '清明节', '劳动节', '端午节', '中秋节', '国庆节'],
+  'ds': pd.to_datetime(['2020-01-24', '2020-04-04', '2020-05-01', '2020-06-03', '2020-09-21', '2020-10-01']),
+})
+
 # 创建并训练模型
-model = Prophet()
+model = Prophet(seasonality_mode='multiplicative', n_changepoints=50, holidays=holidays)
 model.fit(df)
 
+
 # 创建未来日期
-future = model.make_future_dataframe(periods=7)
+future = model.make_future_dataframe(periods=30)
 
 # 生成预测结果
 forecast = model.predict(future)
 
 # 查看预测结果
-print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(7))
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(30))
 
 # 绘制预测图表
 model.plot(forecast)
